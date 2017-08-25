751cf478d3fa7232c51a77c06092528b914f5582
DarioXavier <d_ario13@live.com>
2017-08-25 18:16:05 -0500

    sesion 2

--- js/app.js
+++ js/app.js
@@ -1,7 +1,28 @@
-(function(){
-    'use strict';
+angular.module('MyApp',[]);
 
-    angular
-        .module('MyApp',[])
+angular.module('MyApp').controller('MyCtrl', function($scope){
+    $scope.sms = 'Hola mundo';
+});
 
-}());
+angular.module('MyApp').controller('MyCtrl2', function($scope){
+    //$scope.sms = 'Hola mundo :D';
+});
+
+angular.module('MyApp').controller('MyCtrl3', function($scope){
+    $scope.num1 = 0;
+    $scope.num2 = 0;
+    $scope.resp = 0;
+
+    $scope.sumar = function() {
+        $scope.resp = $scope.num1 + $scope.num2;
+    }
+
+    $scope.incrementar = function() {
+        $scope.num1 = $scope.num1 + 1;
+    }
+
+    $scope.restar = function() {
+        $scope.num1 = $scope.num1 + 1;
+    }
+
+});
