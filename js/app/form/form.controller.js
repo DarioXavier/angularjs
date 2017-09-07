8c459e642adcada37248f3613f39fbbaf711d5e3
DarioXavier <d_ario13@live.com>
2017-09-06 20:20:57 -0500

    sesion10

---
+++ js/app/form/form.controller.js
@@ -0,0 +1,19 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('formModule')
+        .controller('formControlador', formControlador)
+
+    function formControlador(){
+        var formCtrl = this;
+        init();
+
+        function init() {
+            
+            
+        }
+
+    }
+
+}());
