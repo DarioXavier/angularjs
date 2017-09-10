95e9521a4e1780faa80ac297f868d4e6932adf07
DarioXavier <d_ario13@live.com>
2017-09-10 11:44:25 -0500

    sesion3

--- js/app/sumador/sumador.controller.js
+++ js/app/sumador/sumador.controller.js
@@ -1,4 +1,4 @@
-angular.module('sumadorMdl').controller('controlador', function($scope){
+/*angular.module('sumadorMdl').controller('controlador', function($scope){
     $scope.sms = 'Hola mundo';
     $scope.num1 = 0;
     $scope.num2 = 0;
@@ -7,4 +7,24 @@ angular.module('sumadorMdl').controller('controlador', function($scope){
     $scope.sumar = function() {
         $scope.resp = $scope.num1 + $scope.num2;
     }
-});
+});*/
+
+(function() {
+    'use strict';
+
+    angular
+        .module('sumadorMdl')
+        .controller('controlador', controlador);
+
+        controlador.$inject = ['$scope'];
+    function controlador($scope) {
+        scope.sms = 'Hola mundo';
+        $scope.num1 = 0;
+        $scope.num2 = 0;
+        $scope.resp = 0;
+    
+        $scope.sumar = function() {
+            $scope.resp = $scope.num1 + $scope.num2;
+        }
+    }
+})();
